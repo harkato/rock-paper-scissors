9844f2157b78232d8f3b3bacdefcecaf4d8975d9
harkato <92404938+harkato@users.noreply.github.com>
2023-07-05 22:39:14 -0300

    Create function gameRound

--- algorithm.js
+++ algorithm.js
@@ -7,15 +7,41 @@ function getRandomIntInclusive() {
 function getComputerChoice() {
     randomNumber = getRandomIntInclusive();
     if (randomNumber == 1) {
-        return "Rock";
+        return "rock";
 } else if (randomNumber == 2) {
-    return "Paper";
+    return "paper";
 } else {
-    return "Scissors";
+    return "scissors";
 }
+}
+function getPlayerChoice() {
+    playerChoice = prompt("Choose your hand");
+    playerSelection = playerChoice.toLowerCase();
+    return playerSelection;
 }
 
+
+computerSelection = getComputerChoice();
+
 function gameRound(playerSelection, computerSelection) {
-    
-}
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors!";
+    } else if (playerSelection === "rock" && computerSelection === "rock"){
+        return "Draw!";
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lose! Paper beats Rock!";
+} else if (playerSelection === "paper" && computerSelection === "rock"){
+    return "You Win! Paper beats Rock!";
+} else if (playerSelection === "paper" && computerSelection === "paper"){
+    return "Draw!";
+} else if (playerSelection === "paper" && computerSelection === "scissors"){
+    return "You Lose! Scissors beats Paper!";
+} else if (playerSelection === "scissors" && computerSelection === "paper"){
+    return "You Win! Scissors beats Paper!";
+} else if (playerSelection === "scissors" && computerSelection === "scissors"){
+    return "Draw!";
+} else {
+    return "You Lose! Rock beats Scissors!";
+}}
 console.log(getComputerChoice())
+console.log(gameRound(playerSelection, computerSelection))
